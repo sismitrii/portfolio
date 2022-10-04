c906c5a4dda7574ac148443e1145bee3b7f1e426
sismitrii <floguerin156@gmail.com>
2022-10-04 13:01:51 +0200

    📱 [Header] : work on responsive header

--- src/component/Header/HeaderScrollBtn.jsx
+++ src/component/Header/HeaderScrollBtn.jsx
@@ -3,8 +3,8 @@ import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
 
 export default function HeaderScrollBtn(){
     return(
-    <a href="#sec-2" className="w-10 h-20 absolute bottom-20 left-1/2 -translate-x-1/2 border-2 border-black rounded-3xl flex justify-center">
-        <FontAwesomeIcon className="relative top-3 text-2xl animate-arrowDown" icon={faAngleDown}/>
+    <a href="#sec-2" className="w-8 sm:w-10 h-16 sm:h-20 absolute bottom-4 sm:bottom-10 left-1/2 -translate-x-1/2 border-2 border-black rounded-3xl flex justify-center">
+        <FontAwesomeIcon className="relative top-2 sm:top-3 text-2xl animate-arrowDown" icon={faAngleDown}/>
     </a>
     )
 }
