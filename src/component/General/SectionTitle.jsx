97b9bd617f4debf2474494e67e3a6f373c8dc5c3
sismitrii <floguerin156@gmail.com>
2022-10-06 12:10:14 +0200

    🎨 [SectionTitle] : new component commun design title

---
+++ src/component/General/SectionTitle.jsx
@@ -0,0 +1,9 @@
+
+export default function SectionTitle({colored, children}){
+    return(
+    <div className="relative w-fit mb-14">
+        <h3 className={`text-5xl font-light mb-5 ${colored ? 'text-navBlue' : 'text-white'}`}>{children}</h3>
+        <div className={`absolute right-0 w-[70px] border-4 ${colored ? 'border-bgBlue' : 'border-navBlue'}`}></div>
+    </div>
+    )
+}
