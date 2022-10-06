d8607d27d3096433150683441f4171ecd4aa1b71
sismitrii <floguerin156@gmail.com>
2022-10-06 12:12:11 +0200

    🎨 [SectionTitle] : replace title by a component

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,11 +1,12 @@
+import SectionTitle from "../General/SectionTitle"
+
 export default function About(){
     return(
         <section id='about'className ='w-full border-b-2 bg-bgBlue text-white flex justify-center items-center'>
             <div className="flex flex-col w-1/2 mx-auto -mt-20">
-                <div className="relative w-fit mb-14">
-                    <h3 className="text-5xl font-light mb-5">ABOUT ME</h3>
-                    <div className="absolute right-0 w-[70px] border-4 border-navBlue"></div>
-                </div>
+                <SectionTitle colored={false}>
+                    ABOUT ME
+                </SectionTitle>
                 <article className='w-[490px] m-auto text-lg '>
                     <p>Je me présente <strong className="text-navBlue font-bolder">Florian</strong>, 26 ans.</p>
                     <p className="mb-6"> Ancien navigateur aérien qui après 6 ans de bons et loyaux services à déçider de se consacrer à 100% à sa passion : le <strong className="text-navBlue font-bolder">Developpement Web</strong> !</p>
