034ac15d232f20bf53a776f01ac6cc7a1b0cda2e
sismitrii <floguerin156@gmail.com>
2022-10-06 20:01:45 +0200

    🚸 [CompetenceCircle] : add appear animation with GSAP

--- src/component/Competence/CompetenceCircle.jsx
+++ src/component/Competence/CompetenceCircle.jsx
@@ -1,12 +1,49 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+import {gsap} from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import {ScrollToPlugin} from 'gsap/ScrollToPlugin'
+import { useEffect } from "react"
+
+gsap.registerPlugin(ScrollTrigger)
 
 export default function CompetenceCircle({text, color, percentage}){
-    const [value, setValue] = useState(percentage);
+    const [value, setValue] = useState(0);
+    const circle = useRef(null);
+
+    const updateValue = () =>{
+        setValue(percentage)
+    }
+
+    const slide = (elem, delay, duration) => {
+        gsap.fromTo(
+            elem,
+            {
+                opacity:0,
+                y: 200
+            },
+            {
+                opacity:1,
+                y:0,
+                onComplete: updateValue,
+                scrollTrigger : {
+                    trigger: elem,
+                    start: 'top bottom'
+                }
+            }
+        )
+    }
+
+
+    useEffect(()=>{
+        slide(circle.current);
+    },[])
+
     return (
     <div>
-        <div className="w-44 mx-24 mt-5 mb-5">
+        <div id={`${text}`} ref={circle} className="w-44 mx-24 mt-5 mb-5">
             <CircularProgressbar 
                 value={value} 
                 text={`${value}`} 
