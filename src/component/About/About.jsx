85b658eb1a722a0eb30dfdaad2e0a46d26cde096
sismitrii <floguerin156@gmail.com>
2022-10-12 14:32:09 +0200

    📱 [About] : make it responsive

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -12,18 +12,24 @@ export default function About(){
 
     return(
         <section id='about'className ='w-full h-[100vh] border-b-2 bg-bgBlue text-white flex justify-center items-center'>
-            <div className="flex flex-col w-1/2 mx-auto" ref={slided}>
+            <div className="flex flex-col w-full p-10 md:w-[800px] xl:w-1/2 mx-auto" ref={slided}>
                 <SectionTitle colored={false}>
                     ABOUT ME
                 </SectionTitle>
-                <article className='w-[490px] m-auto text-lg '>
+                <article className=' w-auto md:w-[550px] m-auto text-base sm:text-lg '>
                     <p>Je me présente <strong className="text-navBlue font-bolder">Florian</strong>, 26 ans.</p>
                     <p className="mb-6"> Ancien navigateur aérien qui après 6 ans de bons et loyaux services à décider de se consacrer à 100 % à sa passion : le <strong className="text-navBlue font-bolder">Developpement Web</strong> !</p>
                     <p>Lors de mes 5 derniers mois en tant que militaire, j'ai pu suivre une formation de développeur Web qui m'a permis d'approfondir mes connaissances. </p>
                     {/* <p>La passion pour l'informatique qui m'anime est de longue date. Je pense que je devais avoir 13 ans la première fois que j'ai "codé" (en C sur) </p>   */}
                     
                 </article>
-                <a href="#contact" id="contact-ancre" className="self-end block w-fit mt-14 p-4 text-xl bg-navBlue hover:bg-white hover:text-navBlue">CONTACT ME !</a>
+                <a 
+                    href="#contact" 
+                    id="contact-ancre" 
+                    className="self-end mt-14 p-4 text-lg md:text-xl bg-navBlue hover:bg-white hover:text-navBlue"
+                    >
+                        CONTACT ME !
+                    </a>
             </div>
         </section>
     )
--- src/component/General/SectionTitle.jsx
+++ src/component/General/SectionTitle.jsx
@@ -2,7 +2,7 @@
 export default function SectionTitle({colored, children}){
     return(
     <div className="relative w-fit mb-14">
-        <h3 className={`text-5xl font-light mb-5 ${colored ? 'text-navBlue' : 'text-white'}`}>{children}</h3>
+        <h3 className={` text-3xl md:text-5xl font-light mb-5 ${colored ? 'text-navBlue' : 'text-white'}`}>{children}</h3>
         <div className={`absolute right-0 w-[70px] border-4 ${colored ? 'border-bgBlue' : 'border-navBlue'}`}></div>
     </div>
     )
