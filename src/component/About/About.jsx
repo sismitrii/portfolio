6160822ae8500c131d78c9a596a03054e3d888aa
sismitrii <floguerin156@gmail.com>
2022-10-14 07:30:34 +0200

    💬 [About] : spelling correction

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -17,8 +17,8 @@ export default function About(){
                     À PROPOS
                 </SectionTitle>
                 <article className=' w-auto md:w-[550px] m-auto text-base sm:text-lg '>
-                    <p className="mb-6"> Ancien navigateur aérien, j'ai décidé, après 6 ans de bons et loyaux services, de me consacrer à 100 % à ma passion : le <strong className="text-navBlue font-bolder">Developpement Web</strong> !</p>
-                    <p>Lors de mes 5 derniers mois en tant que militaire, j'ai suivi une formation qui m'a permis d'obtenir la certification professionnelle <strong className="text-navBlue font-bolder">Developpeur Web RNCP 5</strong> (Bac+2). </p>
+                    <p className="mb-6"> Ancien navigateur aérien, j'ai décidé, après 6 ans de bons et loyaux services, de me consacrer à 100 % à ma passion : le <strong className="text-navBlue font-bolder">Développement Web</strong> !</p>
+                    <p>Lors de mes 5 derniers mois en tant que militaire, j'ai suivi une formation qui m'a permis d'obtenir la certification professionnelle <strong className="text-navBlue font-bolder">Développeur Web RNCP 5</strong> (Bac+2). </p>
                     {/* <p>La passion pour l'informatique qui m'anime est de longue date. Je pense que je devais avoir 13 ans la première fois que j'ai "codé" (en C sur) </p>   */}
                     
                 </article>
