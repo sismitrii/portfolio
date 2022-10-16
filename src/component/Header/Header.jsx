73c17ed375f0286b296b6349785bb1513d0e4bbf
sismitrii <floguerin156@gmail.com>
2022-10-16 18:40:57 +0200

    💄 [header] : change picture and place of text

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,12 +1,12 @@
-import image from '../../assets/images/compressed_header.jpg'
+import image from '../../assets/images/compressed_header2.jpg'
 import HeaderText from './HeaderText'
 import HeaderScrollBtn from './HeaderScrollBtn'
 
 export default function Header(){
     return (
     <header id="accueil" className="w-screen h-screen bg-bgBlue">
-        <img className="w-full h-screen object-cover object-bottom opacity-90" src ={image} alt="Florian Guerin" />
-        <div className='flex flex-col items-center'>
+        <img className="w-full h-screen object-cover opacity-90" src ={image} alt="Florian Guerin" />
+        <div className='flex flex-col items-center xl:items-start ml-[10%]'>
             <HeaderText />
             <HeaderScrollBtn />
         </div>
--- src/component/Header/HeaderText.jsx
+++ src/component/Header/HeaderText.jsx
@@ -2,9 +2,9 @@ import { TypeAnimation } from 'react-type-animation';
 
 export default function HeaderText(){
     return (
-        <div className="absolute min-w-[320px] w-[80%] sm:w-[500px] lg:w-[45%] 2xl:w-[57%] bottom-[10%] sm:bottom-[100px] md:bottom-[150px] 2xl:bottom-[20%]" >
+        <div className="absolute min-w-[320px] w-[80%] sm:w-[500px] lg:w-[45%] 2xl:w-[35%] bottom-[10%] sm:bottom-[100px] md:bottom-[150px] lg:bottom-[15%] xl:bottom-[25%]" >
             <p className=" w-fit relative left-10 text-black text-xl md:text-2xl lg:text-3xl mb-6">Bonjour,</p> {/* Voir pour mettre "Hey !!  👋*/}
-            <TypeAnimation className=' h-20 text-lg md:text-3xl 2xl:text-6xl'
+            <TypeAnimation className=' h-20 text-lg md:text-3xl 2xl:text-4xl'
                 sequence={['Je suis Florian Guerin, Developpeur Web Junior', 3000, '', 1000]}
                 style={{textTransform: 'uppercase', fontWeight: '500', fontFamily: 'Azeret Mono'}}
                 wrapper="h1"
