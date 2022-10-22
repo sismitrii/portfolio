7d1eb7cdbc8a2800dcebf46f9e79bb1d6f45dd83
sismitrii <floguerin156@gmail.com>
2022-10-22 09:59:58 +0200

    💄 [header] : change UI with black overlay and white scripting

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -5,8 +5,8 @@ import HeaderScrollBtn from './HeaderScrollBtn'
 export default function Header(){
     return (
     <header id="accueil" className="w-screen h-screen bg-bgBlue">
-        <img className="w-full h-screen object-cover opacity-90" src ={image} alt="Florian Guerin" />
-        <div className='flex flex-col items-center xl:items-start ml-[10%]'>
+        <img className="w-full h-screen object-cover opacity-60" src ={image} alt="Florian Guerin" />
+        <div className='flex flex-col items-center xl:items-start ml-[10%] text-white'>
             <HeaderText />
             <HeaderScrollBtn />
         </div>
--- src/component/Header/HeaderScrollBtn.jsx
+++ src/component/Header/HeaderScrollBtn.jsx
@@ -5,7 +5,7 @@ export default function HeaderScrollBtn(){
     return(
     <button 
         onClick={()=>document.querySelector('#about').scrollIntoView({behavior:"smooth"})} 
-        className="w-8 xl:w-10 h-16 xl:h-20 absolute bottom-4 xl:bottom-10 left-1/2 -translate-x-1/2 border-2 border-black rounded-3xl flex justify-center">
+        className="w-8 xl:w-10 h-16 xl:h-20 absolute bottom-4 xl:bottom-10 left-1/2 -translate-x-1/2 border-2 border-white rounded-3xl flex justify-center">
         <FontAwesomeIcon className="relative top-2 sm:top-3 text-2xl animate-arrowDown" icon={faAngleDown}/>
     </button>
     )
